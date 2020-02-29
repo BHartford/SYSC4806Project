989b16eb2b1b39ca937833c663f78282270b6aeb
ckirbs <callum.kirby@carleton.ca>
2020-02-29 15:04:25 -0500

    Simple Controller and HTML placeholders

---
+++ src/main/java/webContent/BookStoreController.java
@@ -0,0 +1,37 @@
+package webContent;
+
+import dataModel.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class BookStoreController {
+
+    @Autowired
+    private BookRepository repository;
+
+    @GetMapping("/")
+    public String landingPage(Model model){
+        return index(model);
+    }
+
+    @GetMapping("/view")
+    public String index(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "index";
+    }
+
+    @GetMapping("/viewbook")
+    public String display(Model model, @RequestParam(value = "bookName", defaultValue = "Book") String bookTitle) {
+        model.addAttribute("books", repository.findByTitle(bookTitle));
+        return "viewbook";
+    }
+
+    @PostMapping("/addbook")
+    public String display(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "addbook";
+    }
+}
