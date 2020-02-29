06c1efb5f0bf3674ce2a224b0936c4f914536b83
Callum KIrby <callum.kirby@carleton.ca>
2020-02-29 18:30:55 -0500

    DB Persisted (#17)
    * Added postgreSQL to have persistent storage of books
    
    * Slight modify to AccessBook to match what we did in the lab
    Removed Test so there is only one main method
    
    * Heroku DB info *HARDCODED*
    
    * Simple Controller and HTML placeholders
    
    * Update .travis.yml
    
    * Simple Controller and HTML placeholders
    
    * Corrected Issue with Postgres and base HTML set up
    
    Folder structure was causing for unexpected handling of DB, Controller migrated into dataModel.
    
    Basic HTML pages have been configured to get an Idea of what information will be contained on each page.
    
    TODO: Remove hardcoded Postgres (possibly separate into PROD (hidden) and DEV branches)
    
    * Corrected Issue with Postgres and base HTML set up
    
    Folder structure was causing for unexpected handling of DB, Controller migrated into dataModel.
    
    Basic HTML pages have been configured to get an Idea of what information will be contained on each page.
    
    TODO: Remove hardcoded Postgres (possibly separate into PROD (hidden) and DEV branches)
    
    * Update BookStoreController.java
    
    Co-authored-by: Christopher Molnar <cmolnar22@gmail.com>
    Co-authored-by: BrandonHartford <hartbrand99@gmail.com>

---
+++ src/main/java/dataModel/BookStoreController.java
@@ -0,0 +1,37 @@
+package dataModel;
+
+import dataModel.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class BookStoreController {
+
+    @Autowired
+    private BookRepository repository;
+
+    @GetMapping("/")
+    public String landingPage(Model model){
+        return index(model);
+    }
+
+    @GetMapping("/view")
+    public String index(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "index";
+    }
+
+    @GetMapping("/viewbook")
+    public String display(Model model, @RequestParam(value = "bookID") long bookID) {
+        model.addAttribute("books", repository.findById(bookID));
+        return "viewbook";
+    }
+
+   @PostMapping("/addbook")
+    public String display(Model model, @RequestBody Book newBook) {
+        repository.save(newBook); //Validation?
+        return "addbook";
+    }
+}
