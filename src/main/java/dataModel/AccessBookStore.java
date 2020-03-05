b24ba03d3975f1c6f14a54b9beff632568169bf6
christophermolnar <cmolnar22@gmail.com>
2020-03-05 13:19:04 -0500

    Renamed AccessBook to AccessBookStore to better reflect what it does

--- src/main/java/dataModel/AccessBookStore.java
+++ src/main/java/dataModel/AccessBookStore.java
@@ -2,23 +2,21 @@ package dataModel;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 @SpringBootApplication
-public class AccessBook {
+public class AccessBookStore {
 
-    private static final Logger log = LoggerFactory.getLogger(AccessBook.class);
+    private static final Logger log = LoggerFactory.getLogger(AccessBookStore.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(AccessBook.class);
+        SpringApplication.run(AccessBookStore.class);
     }
 
     @Bean
