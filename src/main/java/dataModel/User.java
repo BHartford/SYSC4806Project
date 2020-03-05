b5c89ee9f7ed763e237a8001d65449c8d49d8c7b
Brandon <hartbrand99@gmail.com>
2020-03-05 12:54:34 -0500

    Change table name

--- src/main/java/dataModel/User.java
+++ src/main/java/dataModel/User.java
@@ -1,13 +1,11 @@
 package dataModel;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Table(name="users")
 public class User {
 
     private static final int SELLER = 0;
