e421a37b0e15288de0f7bd9a0e49e45738aa7097
Gaby Hubert <gabyhubert2@gmail.com>
2020-03-27 15:50:19 -0400

    Create review class and repo

---
+++ src/main/java/dataModel/ReviewRepository.java
@@ -0,0 +1,10 @@
+package dataModel;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
+
+public interface ReviewRepository extends CrudRepository<Review, Long> {
+
+	List<Review> findByBookID(@Param("bookID") long id);
+	
+}
